713f5260f31f9436b8d7892038f954c27fa6d6f8
nate1 <njh022@student.usc.edu.au>
2019-09-04 16:11:04 +1000

    Finished Chapter 14, all functionality is complete

---
+++ app/src/main/java/android/bignerdranch/criminalintent/CrimeDbSchema/CrimeDbSchema.java
@@ -0,0 +1,14 @@
+package android.bignerdranch.criminalintent.CrimeDbSchema;
+
+public class CrimeDbSchema {
+    public static final class CrimeTable{
+        public  static final String NAME = "crimes";
+
+        public class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
